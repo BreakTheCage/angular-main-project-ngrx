8c77a5d27339d73142ff8d6a800411adc834a97e
SaeedYaghuti <sarbaztanha707@gmail.com>
2019-06-22 09:34:24 +0400

    #Completed Section 8; Episode 102

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, HostListener, HostBinding } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective {
+    //Close Dropdown only by clicking on bottom
+    // @HostBinding('class.open') isOpen = false;
+    // @HostListener('click') toggleOpen(){
+    //     this.isOpen = !this.isOpen;
+    // }
+
+    //OR
+    //Close Dropdown by clicking other area in document
+    @HostBinding('class.open') isOpen = false;
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+        console.log('====================================');
+        console.log(this.elRef.nativeElement);
+        console.log('====================================');
+        // this.isOpen = !this.isOpen ;
+    }
+    constructor(private elRef: ElementRef) { }
+    
+} 
