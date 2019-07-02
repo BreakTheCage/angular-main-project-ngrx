f9cf57a673fa8ece9583f9494d3ebd72b5278a04
BreakTheCage <sarbaztanha707@outlook.com>
2019-07-02 22:00:00 +0400

    #On Going

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -36,6 +37,7 @@ import { RecipeService } from './recipes/recipe.service';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService, RecipeService],
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,5 @@
+import { HttpClient } from 'selenium-webdriver/http';
+import { Injectable } from '@angular/core';
+@Injectable({providedIn: 'root'})
+export class DataStorageService implements Http {
+}
