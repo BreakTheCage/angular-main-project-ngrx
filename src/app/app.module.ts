54e717fa1d969559301c57bab61b01b044adf6eb
BreakTheCage <sarbaztanha707@outlook.com>
2019-07-07 12:19:00 +0400

    #331; More Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,9 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';import { AppRoutingModule } from './app-routing.module';import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { HeaderComponent } from './header/header.component';import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -18,8 +16,6 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ShoppingListModule,
-    AuthModule,
     SharedModule,
     CoreModule
   ],
