0afa54c115f0e59fb22bfe8bb401a2e6a953fc71
SaeedYaghuti <sarbaztanha707@gmail.com>
2019-07-08 22:57:29 +0400

    #somthing

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -5,6 +5,5 @@ export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
 export class AddIngredient implements Action {
     readonly type = ADD_INGREDIENT;
-
     constructor(public payload: Ingredient){};
 }
