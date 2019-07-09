fd066e744e96fd12bb04654a7cfe7f01ef3d0258
BreakTheCage <sarbaztanha707@outlook.com>
2019-07-09 15:16:53 +0400

    #363; And Important note on Action

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
-import { log } from 'util';
+import { Store } from '@ngrx/store';
+
+import * as formApp from '../store/app.reducer';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-header',
@@ -13,15 +16,20 @@ import { log } from 'util';
 export class HeaderComponent implements OnInit, OnDestroy {
     isAuthenticated = false;
     private userSub: Subscription;
-    constructor(private dataStorageService: DataStorageService,
-    private authService: AuthService) { }
+    constructor(
+        private dataStorageService: DataStorageService,
+        private authService: AuthService,
+        private store: Store<formApp.AppState>) { }
 
     ngOnInit(): void { 
         //if we have user It means we are Authenticated
-        this.userSub = this.authService.user.subscribe(user => {
-            console.log('Inside ngOninit authService.user.subscribe');
-            this.isAuthenticated = !!user;
-        })
+        // this.userSub = this.authService.user.subscribe(user => {
+        this.userSub = this.store.select('auth')
+            .pipe(map(authState =>  authState.user))
+            .subscribe(user => {
+                console.log('Inside ngOninit authService.user.subscribe');
+                this.isAuthenticated = !!user;
+            })
     }
 
     onSaveData() {
